85dadbccfd4483aebb8eae6c0a36d07321eabc43
tanisu <walk.away.renee.5125@gmail.com>
2021-08-09 22:49:06 +0900

    温度・時計
    時計の更新
    温度の更新

--- Assets/Scripts/System/UIController.cs
+++ Assets/Scripts/System/UIController.cs
@@ -11,17 +11,18 @@ public class UIController : MonoBehaviour
     [SerializeField] Text clothesText;
     [SerializeField] Slider hpSlider;
     [SerializeField] Image timerImage;
+    [SerializeField] Image ondoImage;
 
 
-    public void UpdateHPSlider(int time)
-    {
-        if((hpSlider.maxValue - time) < 0)
-        {
-            return;
-        }
-        hpSlider.value = hpSlider.maxValue - time;
+    //public void UpdateHPSlider(int time)
+    //{
+    //    if((hpSlider.maxValue - time) < 0)
+    //    {
+    //        return;
+    //    }
+    //    hpSlider.value = hpSlider.maxValue - time;
 
-    }
+    //}
 
     public void UpdateText(int clothCount)
     {
@@ -31,5 +32,14 @@ public class UIController : MonoBehaviour
     public void UpdateTimer(float timer)
     {
         timerImage.DOFillAmount(timer, 0.1f).SetEase(Ease.Linear).SetLink(timerImage.gameObject);
+    }
+
+    public void UpdateOndo()
+    {
+        ondoImage.DOFillAmount(1f, 3f).SetEase(Ease.InCubic).SetLink(ondoImage.gameObject).OnComplete(() =>
+        {
+            GameManager.I.DelCloth();
+            ondoImage.DOFillAmount(0f, 0.5f).SetEase(Ease.InCubic).OnComplete(()=> { PhaseBlockController.isPlayerIn = false; });
+        });
     }
 }

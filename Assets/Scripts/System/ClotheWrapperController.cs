f5c336a1cf80b7331c25011988d90959ed65961e
tanisu <walk.away.renee.5125@gmail.com>
2021-08-13 04:58:55 +0900

    アイコン処理（シンプル）

--- Assets/Scripts/System/ClotheWrapperController.cs
+++ Assets/Scripts/System/ClotheWrapperController.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class ClotheWrapperController : MonoBehaviour
 {
     [SerializeField] GameObject[] getClothes;
     float diff = 23.53f;
     int count = 0;
+    private List<GameObject> currentCloth;
+
+    private void Start()
+    {
+        currentCloth = new List<GameObject>();
+    }
+
     public void ViewCloth(string clothTag)
     {
         foreach(GameObject cloth in getClothes)
@@ -26,8 +34,24 @@ public class ClotheWrapperController : MonoBehaviour
                 }
                 tmp.transform.localPosition = new Vector3(count * diff, y, 1f);
                 tmp.GetComponent<SpriteRenderer>().sortingOrder = count;
+                
+                currentCloth.Add(tmp);
                 count++;
             }
         }
+    }
+
+    public void HideCloth()
+    {
+        
+        if(currentCloth.Count > 0)
+        {
+            GameObject tmp = currentCloth.Last();
+            currentCloth.Remove(currentCloth.Last());
+            Destroy(tmp);
+            count--;
+        }
+        
+        
     }
 }
